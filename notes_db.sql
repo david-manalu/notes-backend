48dcc3fbacb0756b49bee0eec838a3415227c0ea
David Kharis Elio M <davidkhariseliom@gmail.com>
2024-06-16 22:17:01 +0700

    Simple Note Backend

---
+++ notes_db.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 16, 2024 at 05:07 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `notes_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+
+CREATE TABLE `notes` (
+  `id` bigint(20) NOT NULL,
+  `title` text NOT NULL,
+  `datetime` datetime NOT NULL,
+  `note` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `notes`
+--
+
+INSERT INTO `notes` (`id`, `title`, `datetime`, `note`) VALUES
+(6, 'Meeting with Marketing Team Updates', '2024-06-17 09:00:00', 'Discuss the new campaign strategy and budget allocation.'),
+(7, 'Doctor\'s Appointment', '2024-06-18 14:30:00', 'Annual check-up with Dr. Smith at the City Hospital.'),
+(8, 'Project Deadline', '2024-06-20 17:00:00', 'Submit the final report for the environmental impact assessment project.'),
+(9, 'Family Dinner', '2024-06-21 19:00:00', 'Dinner with family at Grandma\'s house. Bring dessert.'),
+(10, 'Gym Session', '2024-06-22 06:00:00', 'Morning workout session with the personal trainer.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
